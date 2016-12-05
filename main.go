a146a5340e2d970b7e39c97469386dd4ed4706ff
Joran Jessurun <joran.jessurun@gmail.com>
2016-12-05 22:16:17 +0100

    Some additions, this will work.. And it will work very quick.

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	"time"
 )
 
+var endian = binary.LittleEndian
+
 func stob(s string, l int) []byte {
 	if len(s) > l {
 		s = s[:l]
@@ -20,20 +22,53 @@ func stob(s string, l int) []byte {
 	return []byte(s)
 }
 
-func header(out io.Writer, fileLabel string) {
+func headerRecord(out io.Writer, fileLabel string) {
 	c := time.Now()
 	out.Write(stob("$FL2", 4))                               // rec_tyoe
 	out.Write(stob("@(#) SPSS DATA FILE - xml2sav 2.0", 60)) // prod_name
-	binary.Write(out, binary.LittleEndian, int32(2))         // layout_code
-	binary.Write(out, binary.LittleEndian, int32(-1))        // nominal_case_size
-	binary.Write(out, binary.LittleEndian, int32(0))         // compression
-	binary.Write(out, binary.LittleEndian, int32(0))         // weight_index
-	binary.Write(out, binary.LittleEndian, int32(-1))        // ncases
-	binary.Write(out, binary.LittleEndian, float64(100))     // bias
+	binary.Write(out, endian, int32(2))                      // layout_code
+	binary.Write(out, endian, int32(-1))                     // nominal_case_size
+	binary.Write(out, endian, int32(0))                      // compression
+	binary.Write(out, endian, int32(0))                      // weight_index
+	binary.Write(out, endian, int32(-1))                     // ncases
+	binary.Write(out, endian, float64(100))                  // bias
 	out.Write(stob(c.Format("02 Jan 06"), 9))                // creation_date
 	out.Write(stob(c.Format("15:04:05"), 8))                 // creation_time
 	out.Write(stob(fileLabel, 64))                           // file_label
 	out.Write(stob("\x00\x00\x00", 3))                       // padding
+}
+
+func variableRecords(out io.Writer) {
+
+}
+
+func encodingRecord(out io.Writer) {
+	binary.Write(out, endian, int32(7))  // rec_type
+	binary.Write(out, endian, int32(20)) // filler
+	binary.Write(out, endian, int32(1))  // size
+	binary.Write(out, endian, int32(5))  // filler
+	out.Write(stob("UTF-8", 5))          // encoding
+}
+
+func terminationRecord(out io.Writer) {
+	binary.Write(out, endian, int32(999)) // rec_type
+	binary.Write(out, endian, int32(0))   // filler
+}
+
+func addVar(name string, kind int) {
+
+}
+
+func clearCase() {
+
+}
+
+func setVar(name, value string) {
+
+}
+
+func writeCase() {
+
 }
 
 func main() {
@@ -48,5 +83,5 @@ func main() {
 	out := bufio.NewWriter(file)
 	defer out.Flush()
 
-	header(out, "Export from example.xsav")
+	headerRecord(out, "Export from example.xsav")
 }
