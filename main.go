c2b1e287fb79f7487a963c1fcfd9ce384d8622eb
Joran Jessurun <A.J.Jessurun@tue.nl>
2016-12-06 14:16:58 +0100

    It does something now.

--- main.go
+++ main.go
@@ -17,17 +17,17 @@ type Label struct {
 }
 
 type Var struct {
-	Name     string
-	Type     byte
-	Width    byte
-	Decimals byte
-	Print    int32
-	Write    int32
-	Label    string
-	Default  string
-	Labels   []Label
-	Value    string
-	HasValue bool
+	Name       string
+	Type       int32
+	Print      byte
+	Width      byte
+	Decimals   byte
+	Label      string
+	Default    string
+	HasDefault bool
+	Labels     []Label
+	Value      string
+	HasValue   bool
 }
 
 var endian = binary.LittleEndian
@@ -36,6 +36,10 @@ type SpssWriter struct {
 	io.Writer
 	Dict    []*Var
 	DictMap map[string]*Var
+}
+
+func NewSpssWriter(w io.Writer) *SpssWriter {
+	return &SpssWriter{Writer: w, DictMap: make(map[string]*Var)}
 }
 
 func stob(s string, l int) []byte {
@@ -52,10 +56,10 @@ func (out *SpssWriter) headerRecord(fileLabel string) {
 	out.Write(stob("$FL2", 4))                               // rec_tyoe
 	out.Write(stob("@(#) SPSS DATA FILE - xml2sav 2.0", 60)) // prod_name
 	binary.Write(out, endian, int32(2))                      // layout_code
-	binary.Write(out, endian, int32(-1))                     // nominal_case_size
+	binary.Write(out, endian, int32(1))                      // nominal_case_size
 	binary.Write(out, endian, int32(0))                      // compression
 	binary.Write(out, endian, int32(0))                      // weight_index
-	binary.Write(out, endian, int32(-1))                     // ncases
+	binary.Write(out, endian, int32(100))                    // ncases
 	binary.Write(out, endian, float64(100))                  // bias
 	out.Write(stob(c.Format("02 Jan 06"), 9))                // creation_date
 	out.Write(stob(c.Format("15:04:05"), 8))                 // creation_time
@@ -64,7 +68,27 @@ func (out *SpssWriter) headerRecord(fileLabel string) {
 }
 
 func (out *SpssWriter) variableRecords() {
-
+	for _, v := range out.Dict {
+		binary.Write(out, endian, int32(2)) // rec_type
+		binary.Write(out, endian, v.Type)   // type (0 or strlen)
+		if len(v.Label) > 0 {
+			binary.Write(out, endian, int32(1)) // has_var_label
+		} else {
+			binary.Write(out, endian, int32(0)) // has_var_label
+		}
+		binary.Write(out, endian, int32(0)) // n_missing_values
+		format := int32(v.Print)<<16 | int32(v.Width)<<8 | int32(v.Decimals)
+		binary.Write(out, endian, format) // print
+		binary.Write(out, endian, format) // write
+		out.Write(stob(v.Name, 8))        // name
+		if len(v.Label) > 0 {
+			binary.Write(out, endian, int32(len(v.Label))) // label_len
+			out.Write([]byte(v.Label))                     // label
+			for i, pad := 0, len(v.Label)%4; i < pad; i++ {
+				out.Write([]byte{0}) // pad out until multiple of 32 bit
+			}
+		}
+	}
 }
 
 func (out *SpssWriter) encodingRecord() {
@@ -117,7 +141,21 @@ func main() {
 	bufout := bufio.NewWriter(file)
 	defer bufout.Flush()
 
-	out := &SpssWriter{Writer: bufout}
+	out := NewSpssWriter(bufout)
 
+	out.addVar(&Var{
+		Name:     "TESTA",
+		Type:     0,
+		Print:    5,
+		Width:    8,
+		Decimals: 2,
+		Label:    "Test label",
+	})
 	out.headerRecord("Export from example.xsav")
+	out.variableRecords()
+	out.encodingRecord()
+	out.terminationRecord()
+	for i := float64(0.0); i < 10; i += 0.1 {
+		binary.Write(out, endian, i)
+	}
 }
